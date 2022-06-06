fb3abc133e4fb6b512525ea79020ac2bc519b66f
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-06-06 22:36:12 +0530

    loops

---
+++ TermsOfAp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TermsOfAp {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int z = 1;
+        for (int i = 1; i <= n; i++) {
+            int output = (3 * z) + 2;
+            if (output % 4 != 0) {
+                System.out.print(output + " ");
+            }
+            z++;
+        }
+    }
+}
