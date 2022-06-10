fa0ff16dfe5594fc35eab916a03fc81d031d38e0
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-06-10 19:24:41 +0530

    more array questions

---
+++ _3_Arrays/_16_secondLargest.java
@@ -0,0 +1,40 @@
+package _3_Arrays;
+
+import java.util.Scanner;
+
+public class _16_secondLargest {
+    public static int[] takeInput() {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int array[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void printArray(int[] array) {
+        int size = array.length;
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = takeInput();
+        int size = arr.length;
+        int largest = -1;
+        int secLargest = -1;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > largest) {
+                secLargest = largest;
+                largest = arr[i];
+            }
+            if (arr[i] > secLargest && arr[i] < largest) {
+                secLargest = arr[i];
+            }
+        }
+        System.out.println(secLargest);
+    }
+}
