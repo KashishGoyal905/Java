ccf8e40667280918a8bba46229d564bf0241aa84
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-06-11 18:35:45 +0530

    searching and sorting

---
+++ _3_Arrays/Searching/_1_linearSearch.java
@@ -0,0 +1,28 @@
+package _3_Arrays.Searching;
+
+import java.util.Scanner;
+
+public class _1_linearSearch {
+    public static int[] takeInput() {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int array[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = takeInput();
+        int size = arr.length;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input the no. to find in the array");
+        int toFind = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == toFind) {
+                System.out.println("index is: " + i);
+            }
+        }
+    }
+}
