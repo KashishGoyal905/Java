34d24aedeafb56b4a6a6427bfb37dcbd5eb8334c
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-06-12 15:21:24 +0530

    strings

---
+++ _4_Strings/_4_reverseString.java
@@ -0,0 +1,11 @@
+package _4_Strings;
+
+import java.util.Scanner;
+
+public class _4_reverseString {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        
+    }
+}
