f31dd1666f6ee1ed453508a35d027cd0a0c7fc9b
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-06-27 19:03:03 +0530

    started oops

---
+++ _6_OOPS/_1_student.java
@@ -0,0 +1,7 @@
+package _6_OOPS;
+
+public class _1_student {
+    String name;
+    int rollNumber;
+
+}
---
+++ _6_OOPS/_2_StudentUse.java
@@ -0,0 +1,10 @@
+package _6_OOPS;
+
+public class _2_StudentUse {
+    public static void main(String[] args) {
+        _1_student s1  = new  _1_student();
+        s1.name = "kii";
+        s1.rollNumber = 100;
+        _1_student s2  = new  _1_student();
+    }
+}
