608a6d39b003c79488a5cf32f8c4bd1904d5fa2f
Kashish Goyal <Kashishgoyal961@gmail.com>
2022-09-07 01:52:31 +0530

    getting started wih linked list

---
+++ _10_linkedList/_1_ArrayList.java
@@ -0,0 +1,18 @@
+package _10_linkedList;
+
+import java.util.ArrayList;
+
+public class _1_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println(list.size());
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(2,50); // it will add ro at index 2 shift all by 1 to the right
+        System.out.println(list.size());
+        System.out.println(list.get(1));
+        list.remove(1);
+        list.set(1, 10);
+    }
+}
